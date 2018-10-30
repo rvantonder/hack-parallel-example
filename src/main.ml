f4d460cd13ab3c2e7acef791d5b68a7c7dbc9274
Rijnard van Tonder <rvantonder@gmail.com>
2018-10-29 20:26:18 -0400

    lpt output

--- src/main.ml
+++ src/main.ml
@@ -6,7 +6,28 @@ let create () =
   scheduler
 
 let () =
+  let files =
+    [ ("a", 3)
+    ; ("b", 4)
+    ; ("c", 4)
+    ; ("d", 2)
+    ; ("e", 3)
+    ; ("f", 9)
+    ; ("g", 2)
+    ]
+  in
+  (**
+     0: [d,e,b,]
+     1: [g,a,c,]
+     2: [f,]
+  *)
   Format.printf "Hello world@.";
+  let partitions = Scheduler.longest_processing_time_first 3 files in
+  List.iteri  (fun i partition ->
+      Format.printf "%d: [" i;
+      List.iter (fun element -> Format.printf "%s," element) partition;
+      Format.printf "]@.")
+    partitions;
   let scheduler = create () in
   Format.printf "Ok world@.";
   Scheduler.destroy scheduler;
